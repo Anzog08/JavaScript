c06e811efabb7b4a01ab7511adaa89fd2aa6beee
Chinzo <chinzorigt003@gmail.com>
2023-11-06 19:15:55 -0800

    java

---
+++ Javascript/intro.js
@@ -0,0 +1,13 @@
+const time = 3;
+
+const minute = time * 60;
+
+console.log(minute, "minute");
+
+const a = 1;
+const b = 2;
+const c = 3;
+if (a > b) console.log("Hello");
+else if (c > b) {
+  console.log(c);
+}
