e6bca74fbbbcc47eebe599bb709cf6d0fa773507
Chinzo <chinzorigt003@gmail.com>
2023-11-07 21:47:31 -0800

    for

--- Javascript/js bodlogo/forbod1.js
+++ Javascript/js bodlogo/forbod1.js
@@ -1,10 +1,10 @@
-let a = 0;
+// let a = 0;
 
-for (let i = 0; i <= 4; i++) {
-  a = a + 3;
-  console.log(a, "A");
-}
-
-// for (let i = 0; i < 28; i++) {
-//   console.log("Pinecone Academy");
+// for (let i = 0; i <= 4; i++) {
+//   a = a + 3;
+//   console.log(a, "A");
 // }
+
+for (let i = 0; i < 28; i++) {
+  console.log("Pinecone Academy");
+}
