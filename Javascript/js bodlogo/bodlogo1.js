7075a0948622a321e50e0af3991d098d04787df2
Chinzo <chinzorigt003@gmail.com>
2023-11-07 21:29:40 -0800

    bodlogo nemev

--- Javascript/js bodlogo/bodlogo1.js
+++ Javascript/js bodlogo/bodlogo1.js
@@ -1,11 +1,11 @@
 const number = 3;
 
-console.log(number,"bol sondgoi bna");
+console.log(number, "bol sondgoi bna");
 
-console.log(number,"bol tegsh bna");
+console.log(number, "bol tegsh bna");
 
-if (){
-    console.log(number, "bol sondgoi bna");
-}else{
-    console.log(number, "bol tegsh bna")}
-    
+if (fhthjh) {
+  console.log(number, "bol sondgoi bna");
+} else {
+  console.log(number, "bol tegsh bna");
+}
